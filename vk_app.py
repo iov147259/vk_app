41bb9b8a8e07e97747a08f572f95171b8b471be8
Maxi, Ivanov <iov147259@gmail.com>
2021-03-30 20:15:44 +0500

    -post url -the subscriber counting system for every day was change

--- vk_app.py
+++ vk_app.py
@@ -79,7 +79,7 @@ while True:
     for g_id in groups:
         count_of_members = get_response(base_url, version, token, "groups.getMembers", "group_id={}".format(g_id))[
             'count']
-        members.append([g_id, count_of_members])
+        members.append([g_id, count_of_members - 1])
 
     # получаем данные о группах
 
@@ -98,15 +98,17 @@ while True:
         groups_info_list.append(list(g.values()))
 
     # получаем статистику групп, котрая имеется до этого момента(исполняется один раз)
+
     if scope == 5:
         groups_stats = []
         groups_stats_list = []
         for g_id in groups:
             groups_stats_list.append([g_id, get_response(base_url, version, token, "stats.get",
                                                          "group_id={}&extended=1&interval = day&stats_groups= visitors, reach, activity&timestamp_from={}&timestamp_to={}".format(
-                                                             g_id, int(time.time()) - 1036800, int(time.time())))])
+                                                             g_id, int(time.time()) - 1900800, int(time.time())))])
 
-        for period_num in reversed(range(len(groups_stats_list[0][1]))):
+        for period_num in range(len(groups_stats_list[0][1])):
+
             for group_list in groups_stats_list:
 
                 if 'activity' in group_list[1][period_num].keys():
@@ -118,19 +120,22 @@ while True:
                     groups_stats.append(
                         [group_list[0], to_date(group_list[1][period_num]["period_from"]), 0, 0, 0, 0, 0])
         for members_arr in members:
-            members_arr[1] = 0
+
             for stats in groups_stats:
+
                 if stats[0] == members_arr[0]:
-                    stats.append(members_arr[1] + int(stats[3]) - int(stats[4]))
-                    members_arr[1] = members_arr[1] + int(stats[3]) - int(stats[4])
+                    stats.append(members_arr[1])
+                    members_arr[1] = members_arr[1] - int(stats[3]) + int(stats[4])
         stats_column_names = ["group_id", "date", "likes", "subscribed", " unsubscribed", "comments", "reposts",
                               "count of subscribers"]
 
-        last_date.append(groups_stats[-1][1])
+        last_date.append(groups_stats[0][1])
         engine = create_engine("postgresql+psycopg2://postgres:{}@localhost/vk_db".format(reader("postpas.txt")))
-        print("processing...")
-        pd.DataFrame(groups_stats, columns=stats_column_names).to_sql('stats_table', con=engine,
-                                                                      if_exists='replace', index=False)
+        print("loading in psql...")
+
+        pd.DataFrame(reversed(groups_stats), columns=stats_column_names).to_sql('stats_table', con=engine,
+                                                                                if_exists='replace', index=False)
+
         print("done!")
         # обновляем инфу раз в 2 часа, дополняя таблицу
     else:
@@ -141,7 +146,7 @@ while True:
                                                          "group_id={}&extended=1&interval = day&stats_groups= visitors, reach, activity&timestamp_from={}&timestamp_to={}".format(
                                                              g_id, int(time.time()) - 7200, int(time.time())))])
 
-        for period_num in reversed(range(len(groups_stats_list[0][1]))):
+        for period_num in range(len(groups_stats_list[0][1])):
             for group_list in groups_stats_list:
 
                 if 'activity' in group_list[1][period_num].keys():
@@ -153,23 +158,24 @@ while True:
                     groups_stats.append(
                         [group_list[0], to_date(group_list[1][period_num]["period_from"]), 0, 0, 0, 0, 0])
         for members_arr in members:
-            members_arr[1] = int(members_arr[1])-1
+
             for stats in groups_stats:
                 if stats[0] == members_arr[0]:
-                    stats.append(members_arr[1] + int(stats[3]) - int(stats[4]))
-                    members_arr[1] = members_arr[1] + int(stats[3]) - int(stats[4])
+                    stats.append(members_arr[1])
+
         stats_column_names = ["group_id", "date", "likes", "subscribed", " unsubscribed", "comments", "reposts",
                               "count of subscribers"]
         # проверяем, если пришёл ответ, в котором дата статистики, такая же как у последней записи, если так, то удаляем записи с такой же датой и добавляем принятые данные
 
-        if groups_stats[0][1] == last_date[0]:
+        if groups_stats[-1][1] == last_date[0]:
             print("update")
             con = psycopg2.connect(database="vk_db", user='postgres', password=reader("postpas.txt"), host="localhost",
                                    port=5432)
-            cur = con.cursor()
-            cur.execute("DELETE FROM stats_table WHERE date= %s", (last_date[0],))
-            con.commit()
-            con.close
+            with con as con:
+                cur = con.cursor()
+                cur.execute("DELETE FROM stats_table WHERE date= %s", (last_date[0],))
+                con.commit()
+
             engine = create_engine("postgresql+psycopg2://postgres:{}@localhost/vk_db".format(reader("postpas.txt")))
 
             pd.DataFrame(groups_stats, columns=stats_column_names).to_sql('stats_table', con=engine,
@@ -178,7 +184,7 @@ while True:
             # если принятые данные содержат дату, которой еще не было, принятые данные просто вносятся в таблицу и меняется значение переменной last_date, хранящей дату самой свежей записи
         else:
             print("supplement")
-            pd.DataFrame(groups_stats, columns=stats_column_names).to_sql('stats_table', con=engine,
+            pd.DataFrame(reversed(groups_stats), columns=stats_column_names).to_sql('stats_table', con=engine,
                                                                           if_exists='append', index=False)
             last_date[0] = groups_stats[0][1]
             print("done!")
@@ -188,6 +194,7 @@ while True:
     for group_id in groups:
         groups_posts += get_response(base_url, version, token, "wall.get",
                                      "owner_id=-{}&count=20&extended=1&fields= id, name".format(group_id))['items']
+
     # приводим данные о постах в удобоваримыый вид
     list_of_properties = ['from_id', 'date', 'post_type', 'text']
     new_posts_list = []
@@ -198,22 +205,26 @@ while True:
                 post_dict[properties] = post[properties]
             else:
                 post_dict['date of publication '] = to_date(post[properties])
-
+        post_dict["post id"] = post['id']
+        if str(post_dict["from_id"])[0] != '-':
+            post_dict["from_id"] = '-' + str(post_dict["from_id"])
+        post_dict['post link'] = "https://vk.com/wall{}_{}".format(post_dict['from_id'], post_dict['post id'])
         post_dict['comments'] = post['comments']['count']
         post_dict['reposts'] = post['reposts']['count']
         post_dict['likes'] = post['likes']['count']
         if 'copy_history' in post.keys():
 
             post_dict['photo'] = post['copy_history'][0]['attachments'][0]['photo']['sizes'][4]['url']
-        else:
+        elif 'attachments' in post.keys():
 
             post_dict['photo'] = post['attachments'][0]['photo']['sizes'][4]['url']
 
         new_posts_list.append(post_dict)
+    # здесь получаем и обрабатываем  статистику по постам
 
     engine = create_engine("postgresql+psycopg2://postgres:{}@localhost/vk_db".format(reader("postpas.txt")))
     # сгружаем таблицу с группами в psql
-    print("processing...")
+    print("loading to plsq...")
     pd.DataFrame(groups_info_list,
                  columns=["group_id", "group_name", "is_closed", "group_type", "is_advertiser", "group_photo"]).to_sql(
         'groups_info_table', con=engine, if_exists='replace', index=False)
@@ -222,5 +233,5 @@ while True:
 
     print("done!")
     print("slipping now...")
-    time.sleep(30)
+    time.sleep(60)
     print("waking up...")
